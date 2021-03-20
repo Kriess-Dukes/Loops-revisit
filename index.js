9f288876f56bb2fb4e0f42e232cd4e11a44402ee
Kriess Dukes <kdukes282@noscihigh.org>
2021-03-20 00:49:12 +0000

    I edit my coding so that its correct.

--- index.js
+++ index.js
@@ -21,15 +21,18 @@ evenPrint(13) => 2,4,6,8,10,12
 function evenPrint(number){
   //CODE BELOW HERE
  
- var evenPrint(29)
-  for (let i = 2; i <= evenPrint +2; i++)
-
-  console.log(evenPrint{i}) 
+for(var i = 0; <= number; i++){
+ 
+ if(i % 2 === 0){
+   
+   conssole.log(i) 
+ }
+ }
 
 
   //CODE ABOVE HERE
 }
-
+evenPrint(190)
 
 
 
@@ -59,14 +62,17 @@ oddPrint(12) => 1,3,5,7,9,11
 function oddPrint(number){
   //CODE BELOW HERE
  
- var oddPrint(35)
-  for (let i = 1; i <= evenPrint +2; i++)
-
-  console.log(oddPrint{i}) 
+for(var i = 0; <= number; i++){
+ 
+ if(i % 2 !== 0){
+   
+   conssole.log(i) 
+ }
+ }
 
   //CODE ABOVE HERE
 }
-
+oddPrint(28)
 
 
 
@@ -88,11 +94,18 @@ evenOdd(5) => even, odd, even, odd, even, odd
 
 function evenOdd(number){
   //CODE BELOW HERE
-
+for(var i = 0; <= number; i+-){
+ 
+ if(i % 1 === 0){
+   
+   conssole.log(i) 
+ }
+ }
 
   //CODE ABOVE HERE
 }
 
+evenOdd(43)
 
 
 
@@ -111,18 +124,20 @@ countUp(10) => 1,2,3,4,5,6,7,8,9,10
 
 function countUp(number){
   //CODE BELOW HERE
-var myArray = [0,1,2,3,4,5,6,7,8]
-}
 function printArrayValues(array) {
 
-  //write a for loop to ilterate over input array
-  for(let i = 0; i <= array.length -1; i++)
-
-  console.log(array[i])
+for(var i = 0; <= number; i++){
+ 
+ if(i += 1 === 0){
+   
+   conssole.log(i) 
+ }
+ }
 
   //CODE ABOVE HERE
 }
 
+countUp(8)
 
 
 /*
@@ -139,15 +154,15 @@ countDown(10) =>10,9,8,7,6,5,4,3,2,1
 
 function countDown(number){
   //CODE BELOW HERE
-var myArray = [9,8,7,6,5,4,3,2,1,0]
-
-function printArrayValuesInReverse(array) {
- for (let i = array.length - 1; >= 0; i--){
-  console.log(array[i])
-}
-  
-} 
-printArrayValuesInReverse(myArray)
+for(var i = 0; <= number; i++){
+ 
+ if(i - 1 === 0){
+   
+   conssole.log(i) 
+ }
+ }
 
   //CODE ABOVE HERE
 }
+
+countDown(45)
